4c5132ab3fc4215d052912114984f10f6d6f812f
0xthareq <thareqabdulaziz@fmipa.untan.ac.id>
2026-07-23 15:07:43 +0700

    ilkel sidang

--- lib/jurusan-data.ts
+++ lib/jurusan-data.ts
@@ -88,7 +88,7 @@ const formLinks: Record<string, string> = {
   // "ilmu-kelautan-sk-perubahan": "ganti-link-ikl-sk-perubahan", // ← tambah ini
   "ilmu-kelautan-seminar-hasil": "https://forms.gle/GyyUFn6GPBfUCA6w6",
   "ilmu-kelautan-seminar-proposal": "https://forms.gle/1FtZNoWsGGtwFZ2e6",
-  "ilmu-kelautan-sidang": "ganti-link-ikl-sidang",
+  "ilmu-kelautan-sidang": "https://forms.gle/BV1Mh7FT8ZBmLoJE8",
 };
 
 export const jurusanList: Jurusan[] = jurusanBase.map((base) => {
